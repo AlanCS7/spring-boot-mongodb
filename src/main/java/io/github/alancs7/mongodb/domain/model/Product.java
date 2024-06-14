1c8af28b25884c858118d241dd02b3b962ed36ff
Alan Silva <alanclash2541@gmail.com>
2024-06-14 15:15:00 -0300

    feat: Create Java class and Transform the Java class to a collection

---
+++ src/main/java/io/github/alancs7/mongodb/domain/model/Product.java
@@ -0,0 +1,20 @@
+package io.github.alancs7.mongodb.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Builder
+@Document
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    private String id;
+    private String name;
+    private String description;
+
+}
