164b73f275db747efda1975aa502277fe8c13d37
Alan Silva <alanclash2541@gmail.com>
2024-06-16 22:44:54 -0300

    feat: Extend the product model

--- src/main/java/io/github/alancs7/mongodb/domain/model/Product.java
+++ src/main/java/io/github/alancs7/mongodb/domain/model/Product.java
@@ -7,6 +7,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Data
@@ -19,6 +20,9 @@ public class Product {
     private String id;
     private String name;
     private String description;
+    private BigDecimal price;
+    private double quantity;
+    private double rating;
     private List<String> tags;
 
     @DBRef
